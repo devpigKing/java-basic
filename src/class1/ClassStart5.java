61306e0d9e2acc84ee67b0229fef8f9cfc6da511
devpigKing <devpigking@gmail.com>
2023-12-05 09:58:28 +0900

    클래스 개념에 배열 도입(for문을 활용한 리펙토링)

---
+++ src/class1/ClassStart5.java
@@ -0,0 +1,24 @@
+package class1;
+
+public class ClassStart5 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 55;
+
+        Student student2 = new Student();
+        student2.name = "학생1";
+        student2.age = 15;
+        student2.grade = 55;
+
+        // 배열 선언
+        Student[] students = new Student[]{student1, student2};
+
+        // for 문 적용
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("이름:" + students[i].name + " 나이:" +
+                    students[i].age + " 성적:" + students[i].grade);
+        }
+    }
+}
